610030a7ba15df94392709bee45d1c0e958034ca
alexisben <alexiben7@gmail.com>
2022-01-19 13:58:00 +0100

    Cleanup js imports

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,11 +1,11 @@
 // import Alert from 'bootstrap/js/src/alert';
 // import Carousel from 'bootstrap/js/src/carousel';
-import Collapse from 'bootstrap/js/src/collapse';
-import Dropdown from 'bootstrap/js/src/dropdown';
+import 'bootstrap/js/src/collapse';
+import 'bootstrap/js/src/dropdown';
 // import Modal from 'bootstrap/js/src/modal';
 // import Offcanvas from 'bootstrap/js/src/offcanvas';
 // import Popover from 'bootstrap/js/src/popover';
-import Scrollspy from 'bootstrap/js/src/scrollspy';
+import 'bootstrap/js/src/scrollspy';
 // import Tab from 'bootstrap/js/src/tab';
 // import Toast from 'bootstrap/js/src/toast';
 // import Tooltip from 'bootstrap/js/src/tooltip';
