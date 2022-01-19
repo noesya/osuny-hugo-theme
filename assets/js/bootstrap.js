efe4610f9647b9f0fa57e7634c84059a2751580c
sebousan <sebousan@gmail.com>
2022-01-19 13:53:25 +0100

    js

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,3 +1,11 @@
-import Collapse from 'bootstrap/js/dist/collapse';
-import Dropdown from 'bootstrap/js/dist/dropdown';
-import Scrollspy from 'bootstrap/js/dist/scrollspy';
+// import Alert from 'bootstrap/js/src/alert';
+// import Carousel from 'bootstrap/js/src/carousel';
+import Collapse from 'bootstrap/js/src/collapse';
+import Dropdown from 'bootstrap/js/src/dropdown';
+// import Modal from 'bootstrap/js/src/modal';
+// import Offcanvas from 'bootstrap/js/src/offcanvas';
+// import Popover from 'bootstrap/js/src/popover';
+import Scrollspy from 'bootstrap/js/src/scrollspy';
+// import Tab from 'bootstrap/js/src/tab';
+// import Toast from 'bootstrap/js/src/toast';
+// import Tooltip from 'bootstrap/js/src/tooltip';
