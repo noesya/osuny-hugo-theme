1bb3cc4365b691c7a9dc121baba02d16764bd4ad
alexisben <alexiben7@gmail.com>
2022-05-17 17:30:54 +0200

    Key figures and posts block fix

---
+++ assets/js/theme/blocks/keyFigures.js
@@ -0,0 +1,14 @@
+window.blocks = window.blocks || {}
+
+window.blocks.keyFigures = function () {
+    var keyFigures = document.querySelectorAll('.block-key_figures');
+
+    if (keyFigures.length <= 0) {
+        return;
+    }
+
+    window.addEventListener('load', () => {
+        
+    });
+    
+};
