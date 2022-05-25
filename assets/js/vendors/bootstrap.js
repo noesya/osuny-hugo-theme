ab4c9d1e104f4cf5ce7aa91a72f17acd08de8977
sebousan <sebousan@gmail.com>
2022-05-25 10:36:16 +0200

    wip

--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -3,7 +3,7 @@
 import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
 import 'bootstrap/js/src/modal';
-// import Offcanvas from 'bootstrap/js/src/offcanvas';
+import 'bootstrap/js/src/offcanvas';
 // import Popover from 'bootstrap/js/src/popover';
 import 'bootstrap/js/src/scrollspy';
 // import Tab from 'bootstrap/js/src/tab';
