3cab7a62640a5441d9a95adbd4135e476fd98e07
sebousan <sebousan@gmail.com>
2022-06-02 14:44:04 +0200

    nav

--- assets/js/theme/stickyNav.js
+++ assets/js/theme/stickyNav.js
@@ -3,13 +3,16 @@ let previousY = 0,
     y = 0,
     classSticky = 'is-sticky',
     classScrollingDown = 'is-scrolling-down',
-    header = document.querySelector('header[role="banner"]');
+    header = document.querySelector('header[role="banner"]'),
+    offset = header.offsetHeight,
+    toggleMenu = header.querySelectorAll('[data-bs-toggle="dropdown"]'),
+    dropdownMenu = header.querySelectorAll('.dropdown-menu');
 
 events.forEach((event) => {
     window.addEventListener(event, () => {
         y = window.scrollY;
 
-        if (y > header.offsetHeight) {
+        if (y > offset) {
             header.classList.add(classSticky);
         } else {
             header.classList.remove(classSticky);
@@ -17,6 +20,16 @@ events.forEach((event) => {
 
         if (y > previousY) {
             document.documentElement.classList.add(classScrollingDown);
+
+            // Ugly: need to clean by alexis :)
+            for (var i = 0; i < toggleMenu.length; i++) {
+                toggleMenu[i].classList.remove("show");
+                toggleMenu[i].setAttribute("aria-expanded", false);
+            }
+            for (var i = 0; i < dropdownMenu.length; i++) {
+                dropdownMenu[i].classList.remove("show");
+            }
+
         } else {
             document.documentElement.classList.remove(classScrollingDown);
         }
