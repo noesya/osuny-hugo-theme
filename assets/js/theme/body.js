b6f960a4fd0f143e5f92dae93a2a37f4825fafc8
alexisben <alexiben7@gmail.com>
2022-06-09 11:07:12 +0200

    Fix scrollspy

--- assets/js/theme/body.js
+++ assets/js/theme/body.js
@@ -1,3 +1,12 @@
+import ScrollSpy from 'bootstrap/js/src/scrollspy';
+
 window.addEventListener('load', () => {
     document.body.classList.add('is-loaded');
 });
+
+if (document.querySelector('#nav-toc')) {
+    new ScrollSpy(document.body, {
+        target: '#nav-toc',
+        offset: 5
+    });
+}
--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -5,7 +5,7 @@ import 'bootstrap/js/src/dropdown';
 import 'bootstrap/js/src/modal';
 // import Offcanvas from 'bootstrap/js/src/offcanvas';
 // import Popover from 'bootstrap/js/src/popover';
-import 'bootstrap/js/src/scrollspy';
+// import 'bootstrap/js/src/scrollspy';
 // import Tab from 'bootstrap/js/src/tab';
 // import Toast from 'bootstrap/js/src/toast';
 // import Tooltip from 'bootstrap/js/src/tooltip';
